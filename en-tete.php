41c646c18aa3d3ca4f8ca0c866993103a92336db
Alexis Garon-Michaud <alexis_35115@hotmail.com>
2021-03-22 17:43:16 -0400

    ajouter la création dans l'entête

--- en-tete.php
+++ en-tete.php
@@ -3,6 +3,7 @@
     <nav>
         <div style="float: right;">
             <a href="index.php">Accueil</a>	|
+            <a href="creer-film.php">Créer film</a>	|
             <a href="https://www.cinemagaiete.com/">Cinéma Gaieté</a>
         </div>
     </nav>
